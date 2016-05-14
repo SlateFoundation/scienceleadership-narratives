cf58468187733e78fb5760d3d55a1a8bdfe0bcd1
Chris Alfano <chris@jarv.us>
2016-05-14 14:59:13 -0400

    Add field configs for frontend TermReport model

---
+++ sencha-workspace/packages/scienceleadership-narratives/overrides/TermReport.js
@@ -0,0 +1,16 @@
+Ext.define('ScienceLeadership.narratives.overrides.TermReport', {
+    override: 'Slate.model.TermReport',
+}, function(Report) {
+    Report.addFields([
+        {
+            name: 'Assessment',
+            type: 'string',
+            allowNull: true
+        },
+        {
+            name: 'Grade',
+            type: 'string',
+            allowNull: true
+        }
+    ]);
+});
